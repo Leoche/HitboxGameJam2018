fdd7536d1325ebe6dae8de788684aa9e45b5830b
Léo DESIGAUX <leodesigaux@gmail.com>
2018-04-07 18:58:45 +0200

    Added champis

--- src/entities/player.js
+++ src/entities/player.js
@@ -3,7 +3,7 @@ var Player = function (game, x, y, colliders) {
     //  We call the Phaser.Sprite passing in the game reference
     //  We're giving it a random X/Y position here, just for the sake of this demo - you could also pass the x/y in the constructor
     Phaser.Sprite.call(this, game, x, y, 'champi');
-
+    this.game = game;
     this.anchor.setTo(0.5, 1);
     this.realWidth = this.width;
     this.realHeight = this.height;
@@ -21,6 +21,7 @@ var Player = function (game, x, y, colliders) {
     this.energy = 0;
     this.facing = 1;
     this.colliders = colliders;
+    this.touchingChamp = false;
 
     game.add.existing(this);
 
@@ -36,7 +37,7 @@ var Player = function (game, x, y, colliders) {
       this.scale.x *= -1;
     }
 
-    if(game.input.keyboard.isDown(87) && this.body.onFloor()){ // W
+    if(game.input.keyboard.isDown(87) && (this.body.onFloor())){ // W
       this.energy += 2;
       if(this.energy > 100){
         this.energy = 100;
@@ -57,20 +58,20 @@ var Player = function (game, x, y, colliders) {
       else this.body.velocity.x = 150;
       this.facing = 1;
     }
-    if(game.input.keyboard.isDown(38) && this.body.onFloor()){ // UP
-      this.body.velocity.y = -500;
+    if(game.input.keyboard.isDown(38) && (this.body.onFloor() || this.touchingChamp)){ // UP
+      this.body.velocity.y = -700;
     }
-    if(Math.abs(this.body.velocity.x) > 10 && this.body.onFloor()){
+    if(Math.abs(this.body.velocity.x) > 10 && (this.body.onFloor() || this.touchingChamp)){
       this.animations.play('walk', 10, true);
     }
     else{
-      if(this.body.onFloor()) this.frame = 3;
+      if(this.body.onFloor() || this.touchingChamp) this.frame = 3;
       else this.frame = 6;
     }
 
     // Velocity
     this.body.velocity.x *= 0.97;
-    if (this.body.onFloor()) {
+    if (this.body.onFloor() || this.touchingChamp) {
       this.body.velocity.x *= 0.7;
     }
 
